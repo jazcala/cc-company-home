9dd9532a82fe959f2f63dd064f2ebac5e09bc63b
jazcala <jazcalabrese@gmail.com>
2024-10-08 12:08:58 -0300

    Close the menu after click

--- scripts.js
+++ scripts.js
@@ -6,3 +6,16 @@ function onClickHamb() {
     x.style.display = "block";
   }
 }
+
+const onClickMenuLinks = ()=>{
+  let width = window.screen.width;;
+  let menu = document.getElementById('myLinks')
+
+  if(width <= 480){
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+}
